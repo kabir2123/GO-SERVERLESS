c1b5350e8756cb56becece62704c2ff9455882c7
Kabir Panda <kabir@aifservices.in>
2026-06-01 11:53:12 +0530

    handler function added with documentation

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import(
+	"go-serverless/pkg/handlers"
 	"os"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
 func main(){
@@ -21,7 +24,36 @@ func main(){
 	}
 	//create DynamoDB client using this AWS session
 	//client can be used to perform operations like putItem,GetItem,UpdateItem etc
-	
+
 	dynaClient = dynamodb.New(awsSession)
-	lambda.Start(handler)
+	lambda.Start(handler) //handler is the entry function for every Lambda request/event
+
+
+}
+
+//why not put handler inside main ?
+//main runs only once while creating the client after that all req go to handler 
+//otherwise we will end up having multiple clients coz every time a request comes it first creates a client then goes to the handler
+
+
+
+
+const tableName = "LambdaInGoUser"
+            //input                             //output
+func handler(req events.APIGatewayProxyRequest)(*events.APIGatewayProxyResponse , error){
+	switch req.HTTPMethod{
+	case "GET":
+		return handlers.GetUser(req , tableName , dynaClient)
+	case "POST":
+		return handlers.CreateUser(req , tableName , dynaClient)
+	case "PUT":
+		return handlers.UpdateUser(req , tableName , dynaClient)
+	case "DELETE":
+		return handlers.DeleteUser(req , tableName , dynaClient)
+	
+    default:
+		return handlers.UnhandledMethod()
+	}
+
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go-serverless
 
 go 1.26.3
+
+require (
+	github.com/aws/aws-lambda-go v1.54.0
+	github.com/aws/aws-sdk-go v1.55.8
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
